602cebdf3d647b1ded08b9c2f6f848d1cecb6e55
denfer57 <jordan.fromeyer@gmail.com>
2017-10-12 10:49:49 +0200

    Page d'accueil

---
+++ Banniere.php
@@ -0,0 +1,45 @@
+<div class="row">
+                
+        <div id="banniere">
+                <br/>
+                <div class="col-xs-4">
+                    <div class="padd40">
+                    <form action="" method="post">
+                        <input type="text" name="recherche" id="recherche" placeholder="Recherche de cocktails..." required=""/>
+                        <input id="search" class="btn btn-danger" type="submit" value="Recherche"/>
+                    </form>
+                    </div>
+                </div>
+
+                <form action="" method="post" class="form-horizontal">
+                    <div class="col-xs-offset-2 col-xs-4">
+                    <div class="padd20">
+                        <!-- Username input-->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="ndcco">Nom d'utilisateur</label>  
+                            <div class="col-md-7">
+                                <input id="ndcco" name="ndcco" placeholder="" class="form-control input-md" required="" type="text">
+                            </div>
+                        </div>  
+                    </div>      
+                        <!-- Password input-->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="mdpco">Mot de passe</label>
+                            <div class="col-md-7">
+                                <input id="mdpco" name="mdpco" placeholder="" class="form-control input-md" required="" type="password"/>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-xs-1">
+                        <!-- connection button-->
+                                <input id="connect" type="submit" class="btn btn-danger" value="Connection"/>
+                </form>
+                        <!-- subscribe button-->
+                        <a href="" class="btn btn-warning">Inscription</a>
+                    </div>
+                </div>   
+
+            </div>
+
+</div>
---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+      <title>Cocktaillement</title>
+	  <meta charset="utf-8" />
+	  <link rel="stylesheet" type="text/css" href="global.css" />
+	  <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css"/>
+</head>
+
+<body>
+<?php 
+	include("Banniere.php");
+	include("Donnees.inc.php");
+
+	$dir_nom = 'Photos'; // dossier contenant les photos
+	$dir = opendir($dir_nom) or die('Erreur de listage : le répertoire n\'existe pas'); // on ouvre le contenu du dossier courant
+	$fichier= array(); // on déclare le tableau contenant le nom des images
+	
+	//on recupere le nom des images
+	while($element = readdir($dir)) {
+	    if($element != '.' && $element != '..' && stristr($element,".jpg")) {
+	        if (!is_dir($dir_nom.'/'.$element)) {$fichier[] = $element;}
+	    }
+	}
+
+	//recupere le nom des cocktails sans le .jpg et _
+	for($i=0;$i<sizeof($fichier);$i++){
+		$nom_cocktail[$i] = $fichier[$i];
+		$nom_cocktail[$i] = str_replace(".jpg", "", $nom_cocktail[$i]);
+		$nom_cocktail[$i] = str_replace("_", " ", $nom_cocktail[$i]);
+	} 
+
+	closedir($dir);//fermeture du dossier
+	
+	//affichage des images
+	if(!empty($fichier)){
+		$i = 0;
+	        foreach($fichier as $lien) {
+	            echo "\t\t\t
+	            <div class=\"col-lg-3 col-md-4\">
+	            	<div class=\"cocktail\">
+	            		<figure>
+							<img src=\"$dir_nom/$lien \" alt=\"$nom_cocktail[$i]\"/>
+							<figcaption>$nom_cocktail[$i]</figcaption>
+						</figure>
+					</div>
+				</div>";
+				$i++;
+	        }
+	 }
+?>
+</body>
+</html>
