4790989331a844e266e2d1e2a8e1a34ca353ea49
denfer57 <jordan.fromeyer@gmail.com>
2017-12-03 18:20:42 +0100

    Correction create bdd

--- create_BDD_L3_Jordan_Marwin.php
+++ create_BDD_L3_Jordan_Marwin.php
@@ -1,6 +1,12 @@
 <?php 
-//Connexion propre a votre bdd
-include("connection_bdd.php");
+$pdo = new PDO('mysql:host=localhost', 'root', '', 
+    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+);
+ 
+$request = "CREATE DATABASE IF NOT EXISTS projet_web_l3_info DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci";
+$pdo->prepare($request)->execute();
+
+include ("connection_bdd.php");
 
 //-- phpMyAdmin SQL Dump
 //-- version 4.1.14
@@ -20,7 +26,7 @@ include("connection_bdd.php");
 /*!40101 SET NAMES utf8 */;
 
 //--
-//-- Base de données :  `projet_web_l3_info`
+//-- Base de données :  `projet_web_l3_ino`
 //--
 //-- --------------------------------------------------------
 //--
